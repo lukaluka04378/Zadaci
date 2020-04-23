e6a8d3da68f1c635c493ab569d6a6d77acfbfc45
lukaluka04378 <62738464+lukaluka04378@users.noreply.github.com>
2020-04-23 22:48:03 +0200

    Add files via upload
    tatstatura

--- main.cpp
+++ main.cpp
@@ -1,62 +1,128 @@
 #include <iostream>
-
-#include "Mreza.hpp"
-#include "Test.hpp"
+#include "dinstring.hpp"
 
 using namespace std;
 
-int meni()
-{
-    int n;
-    cout << "Unesite broj za operaciju koju zelite:" << endl;
-    cout << "1. Procesiraj" << endl;
-    cout << "2. Potrebno optimizovati" << endl;
-    cout << "3. Optimizuj" << endl;
-    cin >> n;
-    return n;
-}
+class Tastatura{
+protected:
+    DinString proizvodjac;
+    int brojTastera;
+public:
+    void ispis(){
+        cout << proizvodjac << " " << brojTastera;
+    }
+};
+
+class GejmerskaTastatura : public Tastatura{
+private:
+    int brojPritisaka;
+public:
+    GejmerskaTastatura(){
+        brojPritisaka = 0;
+        proizvodjac = "Red Dragon";
+        brojTastera = 104;
+    }
+    GejmerskaTastatura(int a, DinString b, int c){
+        brojPritisaka = a;
+        proizvodjac = b;
+        brojTastera = c;
+    }
+    GejmerskaTastatura(const GejmerskaTastatura &g){
+        brojPritisaka = g.brojPritisaka;
+        proizvodjac = g.proizvodjac;
+        brojTastera = g.brojTastera;
+    }
+
+    ~GejmerskaTastatura(){}
+
+    void ispis(){
+        Tastatura::ispis();
+        cout << " " << brojPritisaka << endl;
+    }
+    void pritisniTaster(){
+        brojPritisaka += 1;
+    }
+    void drziTaster(int sec){
+        brojPritisaka += sec;
+    }
+};
+
+class NumerickaTastatura : public Tastatura{
+private:
+    int brojPritisakaBrojeva;
+public:
+    NumerickaTastatura(){
+        brojPritisakaBrojeva = 0;
+        proizvodjac = "Logitech";
+        brojTastera = 17;
+    }
+    NumerickaTastatura(int a, DinString b, int c){
+        brojPritisakaBrojeva = a;
+        proizvodjac = b;
+        brojTastera = c;
+    }
+    NumerickaTastatura(const NumerickaTastatura &n){
+        brojPritisakaBrojeva = n.brojPritisakaBrojeva;
+        proizvodjac = n.proizvodjac;
+        brojTastera = n.brojTastera;
+    }
+    ~NumerickaTastatura(){}
+
+    void ispis(){
+        Tastatura :: ispis();
+        cout << brojPritisakaBrojeva << endl;
+    }
+    void pritisniTaster(bool pritisnutiBroj){
+        if(pritisnutiBroj)
+            brojPritisakaBrojeva += 1;
+    }
+    void drziTaster(int sec, bool pritisnutiBroj){
+        if(pritisnutiBroj)
+            brojPritisakaBrojeva += sec;
+    }
+};
+
 int main()
 {
-    Test t(0.9);
-    int n;
-    bool b;
-    do
-    {
-        n = meni();
-        switch(n)
-        {
-        case 1:
-            b = t.procesiraj();
-            break;
-        case 2:
-            b = t.potrebnoOptimizovati();
-            break;
-        case 3:
-            if(t.getStanje() == OPTIMIZACIJA)
-            {
-                cout << "Pritisnite 1 da dodate sloj, a 2 da dodate neuron: ";
-                int i;
-                do
-                {
-                    cin >> i;
-                    if(i == 1)
-                        b = t.optimizuj(DODAJ_SLOJ);
-                    if(i == 2)
-                        b = t.optimizuj(DODAJ_NEURON);
-                    else
-                        cout << "Nepostojeca opcija" << endl;
-                }
-                while(i > 2 || i < 1);
-                break;
-            }
-            else
-                cout<<"Nepostojeca opcija"<<endl;
-        }
-        if(n > 3 || n < 1)
-            cout << "Nepostojeca opcija" << endl;
-        else if(b == true)
-            ispisiTest(t);
-    }
-    while(t.getStanje() != KRAJ);
+    GejmerskaTastatura g1;
+    GejmerskaTastatura g2(0, "Razer", 101);
+    GejmerskaTastatura g3(g1);
+
+    g1.ispis();
+    g2.ispis();
+    g3.ispis();
+
+    g1.drziTaster(30);
+    g2.pritisniTaster();
+
+    g1.ispis();
+    g2.ispis();
+
+    NumerickaTastatura n1;
+    NumerickaTastatura n2(700, "LG", 17);
+    NumerickaTastatura n3(n1);
+
+    n1.ispis();
+    n2.ispis();
+    n3.ispis();
+
+    n1.drziTaster(3,true);
+    n2.pritisniTaster(false);
+
+    n1.ispis();
+    n2.ispis();
+
+    n1.pritisniTaster(false);
+    n2.drziTaster(5,true);
+
+    n1.ispis();
+    n2.ispis();
+
+    n1.drziTaster(8,true);
+    n2.pritisniTaster(false);
+
+    n1.ispis();
+    n2.ispis();
+
     return 0;
 }
